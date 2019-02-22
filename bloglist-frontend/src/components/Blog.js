de21e07184973e257599784d5fed54eeac0f4719
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-22 20:41:55 +0200

    Tehtävä 5.6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const blogStyle = {
+  padding: '0.5em',
+  border: '1px solid grey',
+  cursor: 'pointer'
+}
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  const blogTitleWithAuthor = () => (
+    <div>
+      {blog.title} {blog.author}
+    </div>
+  )
+
+  const compactBlog = () => (
+    <div onClick={toggleExpanded} style={blogStyle}>
+      {blogTitleWithAuthor()}
+    </div>
+  )
+
+  const expandedBlog = () => (
+    <div onClick={toggleExpanded} style={blogStyle}>
+      {blogTitleWithAuthor()}
+      <div>{blog.url}</div>
+      <div>{blog.likes} likes <button>like</button></div>
+      {blog.user ? <div>added by {blog.user.name}</div> : null}
+    </div>
+  )
+
+  return expanded ? expandedBlog() : compactBlog()
+}
 
 export default Blog
